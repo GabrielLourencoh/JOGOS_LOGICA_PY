39aa093aff351097d724a3a503827e8d9e01a191
GabrielLourencoh <gl930551@gmail.com>
2025-05-24 22:13:54 -0300

    Alterando a posição da função 'limpar_tela()', adicionando os comandos do jogo dentro de uma nova função 'giro_da_sorte()' e aumentando o time.spleep de 0.5 segundos para 0.85 segundos.

--- giro_da_sorte.py
+++ giro_da_sorte.py
@@ -2,31 +2,33 @@ import random
 import time
 import os
 
-while True:
-    simbolos = ["🥸", "😎", "🫡", "🥳", "🤠", "🧐", "🤓", "🥶"]
-
-    def limpar_tela():
+def limpar_tela():
         if os.name == "nt":
             os.system("cls")
         else:
             os.system("clear")
+def giro_da_sorte():
+    while True:
+        simbolos = ["🥸", "😎", "🫡", "🥳", "🤠", "🧐", "🤓", "🥶"]
 
-    print("Bem-vindo ao Giro da Sorte!")
-    input("Pressione Enter para girar...")
+        print("Bem-vindo ao Giro da Sorte!")
+        input("Pressione Enter para girar...")
 
-    for i in range(10):
-        a = random.choices(simbolos)
-        b = random.choices(simbolos)
-        c = random.choices(simbolos)
-        limpar_tela()
+        for i in range(10):
+            a = random.choice(simbolos)
+            b = random.choice(simbolos)
+            c = random.choice(simbolos)
+            limpar_tela()
 
-        print(f"| {a} | {b} | {c} |")
-        time.sleep(0.5)
+            print(f"| {a} | {b} | {c} |")
+            time.sleep(0.85)
 
-        print("\nResultado: ")
-        if a == b and b == c:
-            print("🏆 Parabéns! Você mandou bem!")
-        elif a == b or b == c or a == c:
-            print("Quase lá! Dois simbolos iguais.")
-        else:
-            print("Putz! Não foi dessa vez.")
+            print("\nResultado: ")
+            if a == b and b == c:
+                print("🏆 Parabéns! Você mandou bem!")
+            elif a == b or b == c or a == c:
+                print("Quase lá! Dois simbolos iguais.")
+            else:
+                print("Putz! Não foi dessa vez.")
+
+giro_da_sorte()
